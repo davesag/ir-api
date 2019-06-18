f623f3024e6025c944a10e509a7ffc79aece7fd8
Dave Sag <davesag@gmail.com>
2019-06-18 11:42:27 +0530

    #1 extract the reducer

--- src/index.js
+++ src/index.js
@@ -9,14 +9,16 @@ const privateMethods = {
   getOpenOrders
 }
 
+const attachKeys = (...keys) => (acc, elem) => {
+  acc[elem] = privateMethods[elem](...keys)
+  return acc
+}
+
 const ir = (key, secret) =>
   key && secret
     ? {
         ...publicMethods,
-        ...Object.keys(privateMethods).reduce((acc, elem) => {
-          acc[elem] = privateMethods[elem](key, secret)
-          return acc
-        }, {})
+        ...Object.keys(privateMethods).reduce(attachKeys(key, secret), {})
       }
     : publicMethods
 
