11d12be9470dd068d3e5218985f8162cf2be7fb5
Dave Sag <davesag@gmail.com>
2019-06-18 14:11:59 +0530

    #2 made it easier to test

---
+++ src/api/private/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  getOpenOrders: require('./getOpenOrders')
+}
---
+++ src/api/public/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  getAllOrders: require('./getAllOrders'),
+  getFxRates: require('./getFxRates'),
+  getMarketSummary: require('./getMarketSummary'),
+  getOrderBook: require('./getOrderBook'),
+  getRecentTrades: require('./getRecentTrades'),
+  getTradeHistorySummary: require('./getTradeHistorySummary'),
+  getValidLimitOrderTypes: require('./getValidLimitOrderTypes'),
+  getValidMarketOrderTypes: require('./getValidMarketOrderTypes'),
+  getValidOrderTypes: require('./getValidOrderTypes'),
+  getValidPrimaryCurrencyCodes: require('./getValidPrimaryCurrencyCodes'),
+  getValidSecondaryCurrencyCodes: require('./getValidSecondaryCurrencyCodes'),
+  getValidTransactionTypes: require('./getValidTransactionTypes')
+}
