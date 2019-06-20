66ace82743eab8bcd7d583a54122994f974b7ce4
Dave Sag <davesag@gmail.com>
2019-06-20 11:59:51 +0530

    #9 improved error handling

--- src/utils/transformError.js
+++ src/utils/transformError.js
@@ -1,6 +1,23 @@
-const transformError = error => ({
-  code: error.code || 400,
-  message: error.message || 'No error message received'
-})
+const ResponseError = require('../errors/ResponseError')
+const RequestError = require('../errors/RequestError')
+
+const transformError = error => {
+  // console.log(error)
+  const details = error.config ? { url: error.config.url } : {}
+
+  if (error.response) {
+    throw new ResponseError(
+      error.response.data['Message'],
+      error.response.status,
+      details
+    )
+  }
+
+  if (error.request) {
+    throw new RequestError(error.message, details)
+  }
+
+  throw new Error(error.message)
+}
 
 module.exports = transformError
