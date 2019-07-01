e25d1b588e42abd2369e6d51a61470a888f3285a
Dave Sag <davesag@gmail.com>
2019-07-01 17:09:11 +0530

    #29 don't call Error.captureStackTrace if it doesn't exsit

--- src/errors/ResponseError.js
+++ src/errors/ResponseError.js
@@ -1,10 +1,11 @@
+const safeFn = require('../utils/safeFn')
 const RequestError = require('./RequestError')
 
 class ResponseError extends RequestError {
   constructor(message, status, details) {
     super(message, details)
     this.name = this.constructor.name
-    Error.captureStackTrace(this, this.constructor)
+    safeFn(Error.captureStackTrace, this, this.constructor)
     this.status = status || 400
   }
 }
