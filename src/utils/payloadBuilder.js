e19b330ad821969078b0c44e90972c2b8b1274c1
Dave Sag <davesag@gmail.com>
2021-05-19 13:17:28 +1000

    n/a upgraded dependencies and re-prettiered

--- src/utils/payloadBuilder.js
+++ src/utils/payloadBuilder.js
@@ -5,37 +5,39 @@ const {
 const nextNonce = require('./nextNonce')
 const trimEmptyKeys = require('./trimEmptyKeys')
 
-const payloadBuilder = (apiKey, apiSecret) => (path, payload = {}) => {
-  const nonce = nextNonce()
-  const data = trimEmptyKeys({
-    apiKey,
-    nonce,
-    signature: 'placeholder',
-    ...payload
-  })
+const payloadBuilder =
+  (apiKey, apiSecret) =>
+  (path, payload = {}) => {
+    const nonce = nextNonce()
+    const data = trimEmptyKeys({
+      apiKey,
+      nonce,
+      signature: 'placeholder',
+      ...payload
+    })
 
-  const url = `${baseURL}/${path}`
+    const url = `${baseURL}/${path}`
 
-  const message = Object.keys(data)
-    .reduce(
-      (acc, elem) => {
-        if (elem !== 'signature') {
-          const value = data[elem]
-          if (Array.isArray(value)) {
-            acc.push(`${elem}=${data[elem].join(',')}`)
-          } else {
-            acc.push(`${elem}=${data[elem]}`)
+    const message = Object.keys(data)
+      .reduce(
+        (acc, elem) => {
+          if (elem !== 'signature') {
+            const value = data[elem]
+            if (Array.isArray(value)) {
+              acc.push(`${elem}=${data[elem].join(',')}`)
+            } else {
+              acc.push(`${elem}=${data[elem]}`)
+            }
           }
-        }
-        return acc
-      },
-      [url]
-    )
-    .join(',')
+          return acc
+        },
+        [url]
+      )
+      .join(',')
 
-  const signer = createHmac('sha256', Buffer.from(apiSecret, 'utf8'))
-  data.signature = signer.update(message).digest('hex').toUpperCase()
-  return data
-}
+    const signer = createHmac('sha256', Buffer.from(apiSecret, 'utf8'))
+    data.signature = signer.update(message).digest('hex').toUpperCase()
+    return data
+  }
 
 module.exports = payloadBuilder
