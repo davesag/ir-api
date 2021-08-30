fdffc3083ee3c62112cea755d0038acddac3def4
Dave Sag <davesag@gmail.com>
2021-08-30 10:46:47 +1000

    #385 added handler getTradesByOrder

---
+++ src/api/private/getTradesByOrder.js
@@ -0,0 +1,30 @@
+const payloadBuilder = require('../../utils/payloadBuilder')
+const { getTransport } = require('../../utils/transport')
+const { defaultParams } = require('../../defaults')
+const { validateFields } = require('../../validation')
+const isPositiveNumber = require('../../validation/isPositiveNumber')
+
+const validation = {
+  orderGuid: ['isRequired', 'isGuid'],
+  pageIndex: ['isPositiveNumber'],
+  pageSize: [isPositiveNumber(50)]
+}
+
+// https://www.independentreserve.com/products/api#GetTradesByOrder
+const getTradesByOrder = (apiKey, apiSecret) => {
+  const buildPayload = payloadBuilder(apiKey, apiSecret)
+
+  return async ({
+    orderGuid,
+    pageIndex = defaultParams.pageIndex,
+    pageSize = defaultParams.pageSize
+  }) => {
+    const payload = { orderGuid, pageIndex, pageSize }
+    validateFields(payload, validation)
+    const path = 'Private/GetTradesByOrder'
+    const { post } = getTransport()
+    return post(path, buildPayload(path, payload))
+  }
+}
+
+module.exports = getTradesByOrder
