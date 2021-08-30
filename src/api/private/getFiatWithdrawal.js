8aaed6d31762687eba14c3c70e3b991abe2e77b8
Dave Sag <davesag@gmail.com>
2021-08-30 11:01:56 +1000

    #385 added handler getFiatWithdrawal

---
+++ src/api/private/getFiatWithdrawal.js
@@ -0,0 +1,22 @@
+const payloadBuilder = require('../../utils/payloadBuilder')
+const { getTransport } = require('../../utils/transport')
+const { validateFields } = require('../../validation')
+
+const validation = {
+  fiatWithdrawalRequestGuid: ['isRequired', 'isGuid']
+}
+
+// https://www.independentreserve.com/products/api#GetFiatWithdrawal
+const getFiatWithdrawal = (apiKey, apiSecret) => {
+  const buildPayload = payloadBuilder(apiKey, apiSecret)
+
+  return async ({ fiatWithdrawalRequestGuid }) => {
+    const payload = { fiatWithdrawalRequestGuid }
+    validateFields(payload, validation)
+    const path = 'Private/GetFiatWithdrawal'
+    const { post } = getTransport()
+    return post(path, buildPayload(path, payload))
+  }
+}
+
+module.exports = getFiatWithdrawal
